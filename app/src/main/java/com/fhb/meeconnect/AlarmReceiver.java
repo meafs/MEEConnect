e56df8ddc9076475cbb87239180be4c27c396e6b
Abrar Fahim <abrar.fahim@yandex.com>
2020-11-21 23:41:34 +0600

    Test

--- app/src/main/java/com/fhb/meeconnect/AlarmReceiver.java
+++ app/src/main/java/com/fhb/meeconnect/AlarmReceiver.java
@@ -4,15 +4,19 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
+import android.util.Log;
 
 
 public class AlarmReceiver extends BroadcastReceiver {
 
+    public static final String TAG = "AlarmReceiver";
     private Context ctx;
     @Override
     public void onReceive(Context context, Intent intent) {
 
         ctx = context;
+
+        Log.d(TAG, "onReceive");
 
         Intent service = new Intent(context, BirthdayNotifyService.class);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
